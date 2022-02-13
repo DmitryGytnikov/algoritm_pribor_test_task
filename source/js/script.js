fdc044786d9dfc7f12396a50a9152d83e69ad7a2
DmitryGytnikov <gytnikovd@mail.ru>
2022-02-13 03:29:25 +0300

    Добавляет ссылки на все страницы

--- source/js/script.js
+++ source/js/script.js
@@ -90,6 +90,7 @@ $(function () {
   $('.input-box__tel-input').click(function(){
     $(this).setCursorPosition(3);
     }).mask("+7(999) 999-99-99",{autoclear: false});
+
 });
 
 // Закрытие попапа fancybox по Esc
